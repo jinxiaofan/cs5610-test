5eec782ab6418919e859e9ce7b1a514bd4805fa4
jin <jin.xia@husky.neu.edugit config --global user.email jin.xia@husky.neu.edu>
2016-10-19 22:06:26 -0700

    config not working

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
+//require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipaddress = "127.0.0.1";
+var port      = 3000;
 
 app.listen(port, ipaddress);
